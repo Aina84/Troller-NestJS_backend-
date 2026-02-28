30637480e4ca22e87f67d0f9308f96974b9eab18
Aina84 <johnsamuelaina84@gmail.com>
2026-02-28 23:14:21 +0100

    feat : cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors();
+  
   const config = new DocumentBuilder()
       .setTitle('Troller API')
       .setDescription('The Troller API description')
